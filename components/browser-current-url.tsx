ba8cf5c43113d89d67b1a60cdc5a873add389da4
milovangudelj <milovan.gudelj@gmail.com>
2024-03-03 01:48:39 +0100

    lol

--- components/browser-current-url.tsx
+++ components/browser-current-url.tsx
@@ -7,9 +7,14 @@ export const BrowserCurrentUrl = () => {
 
   return (
     <>
-      <span className="flex-none">{window.location.hostname}</span>
+      <span className="flex-none">
+        {typeof window !== "undefined" && window.location.hostname}
+      </span>
       <span className="flex-1 min-w-0 truncate text-white/70">
-        {window.location.host.includes(":") ? `:${window.location.port}` : ""}
+        {typeof window !== "undefined" &&
+          (window.location.host.includes(":")
+            ? `:${window.location.port}`
+            : "")}
         {history.currentEntry()}
       </span>
     </>
