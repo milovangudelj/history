4a88785e4712032b536e406a8cf0f56783d78ef5
milovangudelj <milovan.gudelj@gmail.com>
2024-03-03 23:47:35 +0100

    Spelling mistake

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex-1 flex flex-col items-center justify-center p-3">
-      Yo! Navigate around and try ing the controls in the top right corner.
+      Yo! Navigate around and try using the controls in the top right corner.
     </main>
   );
 }
